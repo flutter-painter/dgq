3bbfba93a937fe11c56c5652eee34314838d3569
pierre <pierre@weebi.com>
2021-06-18 18:03:10 +0200

    1st

--- lib/background.dart
+++ lib/background.dart
@@ -21,3 +21,25 @@ class BackgroundColor extends StatelessWidget {
     );
   }
 }
+
+class BackgroundMain extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      decoration: BoxDecoration(
+        gradient: LinearGradient(
+          colors: [
+            // Color(0xff146db6),
+            Color(0xffE8B8E0),
+            Color(0xffED92EF),
+            Color(0xffc13808),
+            Color(0xffa50811),
+            Color(0xff750a12),
+          ],
+          begin: Alignment.topLeft,
+          end: Alignment.bottomRight,
+        ),
+      ),
+    );
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:animator/animator.dart';
 import 'package:dgq/app_bar.dart';
 import 'package:dgq/tracks.dart';
 import 'package:flutter/material.dart';
@@ -30,43 +31,66 @@ class Home extends StatefulWidget {
   _HomeState createState() => _HomeState();
 }
 
-class _HomeState extends State<Home> {
+class _HomeState extends State<Home> with SingleTickerProviderStateMixin {
+  AnimationController controller;
+  CurvedAnimation curve;
+
+  @override
+  void initState() {
+    super.initState();
+    controller = AnimationController(
+      duration: const Duration(seconds: 5),
+      vsync: this,
+    );
+    curve = CurvedAnimation(
+        parent: controller, curve: Curves.fastLinearToSlowEaseIn);
+    controller.forward();
+  }
+
   @override
   Widget build(BuildContext context) {
     final _width = globals.screenWidth(context);
     return Scaffold(
+      floatingActionButton: InkWell(
+        onTap: () => globals.appNavigator.currentState.push(MaterialPageRoute(
+            builder: (context) =>
+                Tracks(_width, globals.screenHeight(context)))),
+        child: Container(
+          height: _width / 4.7,
+          width: _width / 4.7,
+          child: Animator<double>(
+            duration: Duration(seconds: 4),
+            cycles: 0,
+            curve: Curves.easeInOut,
+            tween: Tween<double>(begin: 15.0, end: 20.0),
+            builder: (context, animatorState, child) => Icon(
+              Icons.audiotrack,
+              size: animatorState.value * 2,
+              color: Colors.white,
+            ),
+          ),
+        ),
+      ),
+      floatingActionButtonLocation: FloatingActionButtonLocation.centerFloat,
       extendBodyBehindAppBar: true,
       appBar: appBar(_width),
       body: Stack(
         children: [
-          BackgroundColor(),
+          BackgroundMain(),
           Center(
-            child: Container(
-              child: Image.asset('assets/band.jpg'),
-              padding: EdgeInsets.fromLTRB(
-                  _width / 20, _width / 20, _width / 20, _width / 20),
-              decoration: BoxDecoration(
-                  color: Colors.white.withOpacity(.2),
-                  borderRadius: BorderRadius.all(Radius.circular(30)),
-                  border: Border.all(
-                      color: Colors.white.withOpacity(.1), width: 1)),
-            ),
-          ),
-          InkWell(
-            onTap: () => globals.appNavigator.currentState.push(
-                MaterialPageRoute(
-                    builder: (context) =>
-                        Tracks(_width, globals.screenHeight(context)))),
-            child: Align(
-              alignment: Alignment.bottomCenter,
-              child: Padding(
-                padding: const EdgeInsets.all(44.0),
-                child: Icon(
-                  Icons.music_note,
-                  color: Colors.white,
-                  size: 44,
-                  semanticLabel: 'listen',
-                ),
+            child: FadeTransition(
+              opacity: curve,
+              child: Container(
+                child: Image.asset('assets/band.jpg'),
+                margin: EdgeInsets.fromLTRB(
+                    _width / 20, _width / 20, _width / 20, _width / 20),
+                padding: EdgeInsets.fromLTRB(
+                    _width / 50, _width / 50, _width / 50, _width / 50),
+                decoration: BoxDecoration(
+                    color: Colors.grey.withOpacity(.2),
+                    borderRadius: BorderRadius.all(Radius.circular(10)),
+                    border: Border.all(
+                        color: Colors.white.withOpacity(.3), width: 1)),
               ),
             ),
           ),
